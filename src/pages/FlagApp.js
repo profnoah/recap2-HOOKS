7bc497931294b46415bf9818638e0f1fb19f0e53
profnoah <noah@clarusway.com>
2021-12-16 23:19:02 +0300

    v2

--- src/pages/FlagApp.js
+++ src/pages/FlagApp.js
@@ -2,28 +2,44 @@ import React, { useState, useEffect } from "react";
 
 const FlagApp = () => {
   const [ulkeler, setUlkeler] = useState([]);
-  console.log(ulkeler);
+  const [hata, setHata] = useState(false);
+
   useEffect(() => {
-    fetch("https://restcountries.com/v3/all")
-      .then((res) => res.json())
+    fetch("https://restcountries.com/v3/al")
+      .then((res) => {
+        if (res.status >= 200 && res.status <= 299) {
+          return res.json();
+        } else {
+          setHata(true);
+        }
+      })
       .then((data) => setUlkeler(data))
       .catch((err) => console.log(err));
   }, []);
 
-  return (
-    <div>
-      <h1>ULKELER</h1>
-      {ulkeler.map((ulke) => {
-        const { name, capital, flags } = ulke;
-        return (
-          <div>
-            <h2>{name.common}</h2>
-            <img src={flags[0]} alt="" />
-          </div>
-        );
-      })}
-    </div>
-  );
+  if (!hata) {
+    return (
+      <div>
+        <h1>ULKELER</h1>
+        {ulkeler.map((ulke) => {
+          const { name, capital, flags } = ulke;
+          return (
+            <div key={name.common}>
+              <h2>{name.common}</h2>
+              <img src={flags[0]} alt="" />
+              <h4>{capital}</h4>
+            </div>
+          );
+        })}
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <h1>!!Hata, Veriler Çekilemedi</h1>
+      </div>
+    );
+  }
 };
 
 export default FlagApp;
